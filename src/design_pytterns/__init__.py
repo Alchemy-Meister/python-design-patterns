18a30c131ed377e2c9d2723b7a9e0adea57ca719
Alchemy-Meister <jesusesma@gmail.com>
2020-08-16 14:50:12 +0200

    hardcode package version

--- src/design_pytterns/__init__.py
+++ src/design_pytterns/__init__.py
@@ -6,10 +6,6 @@
 
 from pkg_resources import DistributionNotFound, get_distribution
 
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    __version__ = '0.1.0'
-
 __author__ = 'Alchemy-Meister'
 __license__ = 'GPL-3.0-or-later'
+__version__ = '0.1.0'
