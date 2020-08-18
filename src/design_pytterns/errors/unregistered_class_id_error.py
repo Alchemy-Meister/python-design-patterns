e392b7d1aeac40697fa1bc8b61beaa382fff7bd1
Alchemy-Meister <jesusesma@gmail.com>
2020-08-18 19:53:36 +0200

    added typing to constructor argument

--- src/design_pytterns/errors/unregistered_class_id_error.py
+++ src/design_pytterns/errors/unregistered_class_id_error.py
@@ -4,10 +4,12 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from collections.abc import Hashable
+
 from .design_pyttern_error import DesignPytternError
 
 class UnregisteredClassIdError(DesignPytternError):
-    def __init__(self, class_id):
+    def __init__(self, class_id: Hashable) -> None:
         super().__init__(
             class_id, message=f'unregistered class id: {repr(class_id)}'
         )
