066c00bcd63153cd6fbd346b2db3bd8c3798f30a
Alchemy-Meister <jesusesma@gmail.com>
2020-08-19 21:35:20 +0200

    added docstring and fixed E302

--- src/design_pytterns/factory/subclass_factory.py
+++ src/design_pytterns/factory/subclass_factory.py
@@ -4,6 +4,8 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+"""Subclass Factory."""
+
 from typing import Type
 
 from design_pytterns._helpers import ConcreteSubclassRegister
@@ -11,7 +13,20 @@ from design_pytterns.interfaces import SubclassIdentificable
 
 from .factory import Factory
 
+
 class SubclassFactory(Factory):
+    """
+    Factory that analizes the subclass hierarchy of the given base class that
+    inherits from `SubclassIdentificable`_ and registers all of the concrete
+    subclasses with their associated class id.
+
+    Parameters
+    ----------
+    base_class: SubclassIdentificable
+        The base class that servers as the starting point of the subclass
+        hierarchy analysis.
+    """
+
     def __init__(self, base_class: Type[SubclassIdentificable]):
         super().__init__(
             ConcreteSubclassRegister(base_class).registered_classes
