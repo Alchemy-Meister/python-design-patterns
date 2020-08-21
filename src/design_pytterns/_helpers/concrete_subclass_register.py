657130bd629a0066635bc3fc71bbd3c02ab18375
Alchemy-Meister <jesusesma@gmail.com>
2020-08-21 15:16:16 +0200

    added license and module docstring

--- src/design_pytterns/_helpers/concrete_subclass_register.py
+++ src/design_pytterns/_helpers/concrete_subclass_register.py
@@ -1,3 +1,11 @@
+#! /usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2020 Alchemy-Meister
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Concrete Subclass Register."""
+
 from collections.abc import Hashable
 from inspect import isabstract
 import logging
