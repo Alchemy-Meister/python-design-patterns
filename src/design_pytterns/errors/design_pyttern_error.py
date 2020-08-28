22720a05ea674e57d9801aad66014330442662a9
Alchemy-Meister <jesusesma@gmail.com>
2020-08-28 19:17:16 +0200

    added docstring

--- src/design_pytterns/errors/design_pyttern_error.py
+++ src/design_pytterns/errors/design_pyttern_error.py
@@ -4,10 +4,29 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+"""Generic package error."""
+
 from typing import Any
 
 
 class DesignPytternError(Exception):
+    """
+    Base class for all design_patterns exceptions.
+
+    Parameters
+    ----------
+    *args : Any
+        Variable length arguments.
+    **kwargs : Any
+        Arbitrary keyword arguments.
+
+    Attributes
+    ----------
+    message : str
+        A descriptive sentence explaining the error.
+
+    """
+
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         self.message = kwargs.pop('message', '')
 
--- src/design_pytterns/errors/unregistered_class_id_error.py
+++ src/design_pytterns/errors/unregistered_class_id_error.py
@@ -4,12 +4,24 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+"""Unregistered class id error."""
+
 from collections.abc import Hashable
 
 from .design_pyttern_error import DesignPytternError
 
 
 class UnregisteredClassIdError(DesignPytternError):
+    """
+    Raised when a factory creates an instance of an unknown class id.
+
+    Parameters
+    ----------
+    class_id : Hashable
+        Unknown hashable that raised the exception.
+
+    """
+
     def __init__(self, class_id: Hashable) -> None:
         super().__init__(
             class_id, message=f'unregistered class id: {repr(class_id)}'
