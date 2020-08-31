77dcf9241fd05448430e2f0b95e7be46c78f6202
Alchemy-Meister <jesusesma@gmail.com>
2020-08-31 21:59:33 +0200

    updated doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, Alchemy-Meister'
 author = 'Alchemy-Meister'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
