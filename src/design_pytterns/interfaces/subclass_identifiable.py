6e6f9d6eba1e8708f8db37caf660ac778c8983ed
Alchemy-Meister <jesusesma@gmail.com>
2021-03-31 23:51:04 +0200

    removed unnecessary call to super __init_subclass__ and mro method

--- src/design_pytterns/interfaces/subclass_identifiable.py
+++ src/design_pytterns/interfaces/subclass_identifiable.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
 
-# SPDX-FileCopyrightText: 2020 Alchemy-Meister
+# SPDX-FileCopyrightText: 2020-2021 Alchemy-Meister
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 """Implementation of SubclassIdentifiable interface-like class."""
 
 from collections.abc import Hashable
-from typing import Any, Optional
+from typing import Optional
 
 from design_pytterns.errors import InvalidClassIdError, UnhashableClassIdError
 
@@ -47,16 +47,11 @@ class SubclassIdentifiable():
 
     """
 
-    def __init_subclass__(
-            cls, class_id: Optional[Hashable] = None, **kwargs: Any
-    ) -> None:
-        super().__init_subclass__(**kwargs)
-
+    def __init_subclass__(cls, class_id: Optional[Hashable] = None) -> None:
         if not isinstance(class_id, Hashable):
             raise UnhashableClassIdError(class_id)
 
-        if SubclassIdentifiable.mro()[0] not in cls.__bases__:
-            if class_id is None:
-                raise InvalidClassIdError()
+        if SubclassIdentifiable not in cls.__bases__ and class_id is None:
+            raise InvalidClassIdError()
 
         cls.CLASS_ID = class_id
