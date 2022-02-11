edfbfd4896afc5c6733118fe3a82757b6f98526f
Alchemy-Meister <jesusesma@gmail.com>
2022-02-11 20:18:27 +0100

    simplified implementation

--- src/design_pytterns/singleton/singleton_meta.py
+++ src/design_pytterns/singleton/singleton_meta.py
@@ -1,28 +1,20 @@
 #! /usr/bin/env python3
 
-# SPDX-FileCopyrightText: 2020-2021 Alchemy-Meister
+# SPDX-FileCopyrightText: 2020-2022 Alchemy-Meister
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 """Singleton metaclass."""
 
-from __future__ import annotations
-
-from typing import Any, Tuple
+from typing import Any
 
 
 class SingletonMeta(type):
     """Metaclass implementation of Singleton-pattern."""
 
-    def __init__(
-            cls, name: str, bases: Tuple[type, ...], mmbs: dict[str, Any]
-    ) -> None:
-        super().__init__(name, bases, mmbs)
-        cls._instance = None
-
     def __call__(cls, *args: Any, **kwargs: Any) -> Any:
-        """Invoke ``__call__`` method only if global instance is `None`."""
-        if cls._instance is None:
+        try:
+            return cls._instance
+        except AttributeError:
             cls._instance = super().__call__(*args, **kwargs)
-
-        return cls._instance
+            return cls._instance
